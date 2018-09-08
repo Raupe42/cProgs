8ee4772fa0b19ff1e3ddc8d4dc53658b24573932
RAUPE-PC\jensh <jens_hoecker@t-online.de>
2018-09-08 13:28:50 +0200

    C65A1g fertig gestellt

--- drittesJahr/C65qsort7.c
+++ drittesJahr/C65qsort7.c
@@ -9,49 +9,51 @@ Task: C65A1g
 
 #define ANZAHL 3
 
-
 struct person_t
 {
-    char vn [20];
-    char nn [20];
+    char vn[20];
+    char nn[20];
     int j, m, t;
 };
-int verglGebT (const struct person_t*, const struct person_t*);
+int verglGebT(const struct person_t *, const struct person_t *);
 
-int main (void)
+int main(void)
 {
-    struct person_t arr [ANZAHL] = {{"Emil", "Meier", 1960, 10, 20}
-                                {"Erwin", "Mueller", 1970, 11, 15}
-                                {"Egon", "Moser", 1980, 10, 24}};
+    struct person_t arr[ANZAHL] = {{"Emil", "Meier", 1960, 10, 20},
+                                   {"Erwin", "Mueller", 1970, 11, 15},
+                                   {"Egon", "Moser", 1980, 10, 24}};
     int i;
-    
-    qsort (arr, ANZAHL, sizeof( struct person_t), verglGebT);
+
+    qsort(arr, ANZAHL, sizeof(struct person_t), verglGebT);
 
     for (i = 0; i < ANZAHL; i++)
     {
-        printf("%s<", arr[i]);
-    }    
+        printf("%i.%i  %s %s\n", arr[i].m, arr[i].t, arr[i].vn, arr[i].nn);
+    }
 }
 
-int verglGebT (const struct person_t * pa, const struct person_t * pb)
+int verglGebT(const struct person_t *pa, const struct person_t *pb)
 {
-   // printf ("%i    %i\n", *pa, *pb);
-  /* while (*pa == *pb)
+    // printf ("%i    %i\n", *pa, *pb);
+    /* while (*pa == *pb)
    {
        pa += sizeof (char);
        pb += sizeof (char);
    }
     return (*pa-*pb);
     */
-    if (*pa->m == *pa->m)
+    /*   if (pa->m == pa->m)
     {
+        printf("-%i-  -%i-\n", pa->m, pb->m);
         return (pa->t - (*pb).t);
     }
-    
-    else
-    {
-        return (*pa->m - *pb->m);
-    }
-    
 
+        return (pa->m - pb->m);
+
+ */
+
+    int a, b;
+    a = pa->m * 100 + pa->t;
+    b = pb->m * 100 + pb->t;
+    return (a - b);
 }
