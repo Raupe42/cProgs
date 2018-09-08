cba9923f4cb1f3de4253cec0ccb0d4a9a2594bc8
RAUPE-SURFACE\jensh <jens_hoecker@t-online.de>
2018-09-08 13:07:22 +0200

    C65 weiter

--- drittesJahr/C65QsortTest.c
+++ drittesJahr/C65QsortTest.c
@@ -8,18 +8,19 @@ int verglint (const int*, const int*);
 
 int main (void)
 {
-    int arr [10] = {7, 3, 5, 2, 4, 1, 9, 8, 1, -2};
+    int arr [10] = {7, 3, 5, 2, 4, 0, 9, 8, 1, -2};
     int i;
 
     qsort (arr, 10, sizeof (int), verglint);
 
     for (i = 0; i < 10; i++)
     {
-        printf("%i", arr[i]);
+        printf("%i<", arr[i]);
     }    
 }
 
 int verglint (const int * pa, const int * pb)
 {
+   // printf ("%i    %i\n", *pa, *pb);
     return (*pa-*pb);
 }
