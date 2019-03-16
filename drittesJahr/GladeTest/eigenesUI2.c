f0af94c1c2ca45a2a5bf1aa6cace07df289ccae8
RAUPE-SURFACE\jensh <jens_hoecker@t-online.de>
2019-03-16 11:19:36 +0100

    eigenesui2 hinzu

--- drittesJahr/GladeTest/eigenesUI2.c
+++ drittesJahr/GladeTest/eigenesUI2.c
@@ -7,7 +7,7 @@ typedef struct gui_struct {
   GtkWidget* switch1;
   GtkWidget* progBar;
   GtkWidget* drawArea1;
-  GtkWidegt* drawArea2;
+  GtkWidget* drawArea2;
 }gui_t;
 
 
@@ -34,11 +34,27 @@ void schalterGeklickt(GtkToggleButton *schalter, gpointer pointer)
   //g_print("Klick %g\n", gtk_progress_bar_get_fraction (GTK_PROGRESS_BAR(p_gui->progBar)));
 }
 
-gboolean zeichnen0 (GtkWidget *flaeche, cairo_t *cr, gui_t gui)
+gboolean zeichnen0 (GtkWidget *flaeche, cairo_t *cr, gpointer muell)
 {
-    
+    cairo_move_to (cr, 0, 0);
+    cairo_line_to(cr, 400, 400);
+    cairo_move_to (cr, 0, 400);
+    cairo_line_to (cr, 400, 0);
+    cairo_stroke(cr); //zeichnen
+    return FALSE;
 }
 
+gboolean zeichnen1 (GtkWidget *flaeche, cairo_t *cr, gui_t gui)
+{
+  guint breite, hoehe;
+  breite = gtk_widget_get_allocated_width (flaeche);
+  hoehe = gtk_widget_get_allocated_height (flaeche);
+   cairo_move_to (cr, 0, 0);
+    cairo_line_to(cr, breite, hoehe);
+    cairo_move_to (cr, 0, hoehe);
+    cairo_line_to (cr, breite, 0);
+    cairo_stroke(cr); //zeichnen
+}
 
 int main(int argc, char *argv[])
 {
@@ -50,7 +66,7 @@ int main(int argc, char *argv[])
   gtk_init(&argc, &argv);
 
   baum=gtk_builder_new();                     
-  gtk_builder_add_from_file(baum, "eigenesUI.ui", NULL); 
+  gtk_builder_add_from_file(baum, "eigenesUI2.ui", NULL); 
 
   element=gtk_builder_get_object(baum, "window1");
   g_signal_connect(element, "delete_event", 
@@ -69,13 +85,14 @@ int main(int argc, char *argv[])
   element = gtk_builder_get_object(baum, "progressbar1");
   gui.progBar = GTK_WIDGET(element);
 
-  gui.drawArea1 = gtk_builder_get_object(baum, "drawingarea1");
+  element = gtk_builder_get_object(baum, "drawingarea1");
+  gui.drawArea1 = GTK_WIDGET (element);
   gui.drawArea2 = gtk_builder_get_object(baum, "drawingarea2");
 
-
-
+  g_signal_connect(G_OBJECT (element), "draw", G_CALLBACK(zeichnen0), NULL);
+  g_signal_connect(G_OBJECT(gui.drawArea2), "draw", G_CALLBACK(zeichnen1), &gui);
   element=gtk_builder_get_object(baum, "window1");
-  gtk_window_maximize(GTK_WINDOW(element));
+  //gtk_window_maximize(GTK_WINDOW(element));
   gtk_widget_show_all(GTK_WIDGET(element));
   gtk_main();
   return 0;
