f06a174547c65be1444c958be2371b2d80a812f5
Raupe42 <jens_hoecker@t-online.de>
2020-03-24 12:43:00 +0100

    etwas schöner gemacht

--- viertesJahr/C72A/a/fragefeld.c
+++ viertesJahr/C72A/a/fragefeld.c
@@ -8,17 +8,17 @@ char fragefeld [MAX_ANZAHL] [2] [81];
             |______________|
                     |
              _______|___________
-            |   Listenelement * |
+            |   Listenelement * |       //erste Dimension
             |___________________|
          ___________|_____________
         |                         |
      ___|_____              ______|______
-    |  Frage  |            |   Antowort  |
-    |_________|            |_____________|
+    |  Frage  |            |   Antowort  |      //zweite Dimension 
+    |_________|            |_____________|      //Frage bei [0] und Antwort bei [1]
         |                         |
      ___|________           ______|______
-    |  Zeichen * |         |  Zeichen *  |
-    |____________|         |_____________|
+    |  Zeichen * |         |  Zeichen *  |      //dritte Dimension -  Da String kein elementarer Datentyp ist
+    |____________|         |_____________|      //sollte diese Dimension vorhanden sein. 
 
 
 
@@ -40,3 +40,19 @@ char fragefeld [MAX_ANZAHL] [2] [81];
      _______|_______
     |   char    *   |      //String
     |_______________|
+
+
+/*
+Wie bereits im letzten Präsenzunterricht thematisiert gibt es eine Definitionslücke
+des Jackson-Diagramms bei mehrdimensionalen Arrays. 
+Aufgrund dieser Definitionslücke ist das Obere Diagramm sowohl fuer die gewaehlte Definition aber 
+auch fuer die Definition als struct fragen {char frage [80]; char antwort [80];} in einem [200] gueltig.
+
+Nach meiner Ansicht stellt die untere Variante allerdings dieses eigentlich linaere dreidimensionale Array of Char
+deutlicher dar. 
+
+Das Jackson-Diagramm ist spracheneutral. Daher ist nicht klar, wie mit Datentypen zu verfahren ist, die nicht in allen Sprachen
+elementar sind. Dies gilt insbesondere fuer Strings - also Zeichenketten. In C sind diese nur ueber Header-Dateien so benannt
+und eigentlich nur ein einfaches Array of Char. Daher wird diese Aufloesung hier einzeln als tiefste Ebene (vgl. Baum -> Blatt)
+betrachtet.
+*/
